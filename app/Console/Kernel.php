38986e8dd219814fbe0fbee37daf3ddc05580004
hsen <hsendeeb2@gmail.com>
2025-12-23 10:34:18 +0200

    adjust scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('subscriptions:expire')->daily();
+        $schedule->command('subscriptions:expire')->everyMinute();
     }
 
     /**
